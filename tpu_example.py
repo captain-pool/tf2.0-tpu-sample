6cd4839d343db599017bbc448645705d4da4c0ec
captain-pool <rickdey1998@gmail.com>
2019-06-05 15:54:10 +0000

    Fixed TPU with TF 2.0 Issue

--- tpu_example.py
+++ tpu_example.py
@@ -13,7 +13,7 @@ flags.DEFINE_integer("iterations",2, "Number of Itertions")
 flags.DEFINE_integer("batch_size", 10, "Size of eahc Batch")
 flags.DEFINE_boolean("use_tpu", True, " Use TPU")
 flags.DEFINE_string("model_dir", "mnist/", "Directory to Save the Models and Checkpoint")
-
+flags.DEFINE_string("data_dir", None, "Directory to Save Data to")
 class MNIST(tf.keras.Model):
     def __init__(self, params):
         super(MNIST, self).__init__()
@@ -40,11 +40,9 @@ class MNIST(tf.keras.Model):
     def call(self, inputs, training=True):
         x = tf.keras.layers.Flatten()(inputs)
         x = self.layer_1(x)
-        x.trainable = training
+        return x
         x = self.layer_2(x)
-        x.trainable = training
         o = self.out(x)
-        o.trainable = training
         return o
 
 
@@ -53,7 +51,9 @@ def input_(mode, batch_size, iterations, **kwargs):
     dataset = tfds.load(
         "mnist",
         as_supervised=True,
-        split="train" if mode == tf.estimator.ModeKeys.TRAIN else "test")
+        split="train" if mode == tf.estimator.ModeKeys.TRAIN else "test",
+        data_dir = kwargs["data_dir"]
+        )
 
     def scale(image, label):
         image = tf.cast(image, tf.float32)
@@ -68,6 +68,11 @@ def input_(mode, batch_size, iterations, **kwargs):
 
 def model_fn(features, labels, mode, params):
     model = MNIST(params)
+    #model = tf.keras.Sequential([
+    #    tf.keras.layers.Flatten(input_shape=[28, 28, 1]),
+    #    tf.keras.layers.Dense(128),
+    #    tf.keras.layers.Dense(10, activation="softmax")
+    #])
     optimizer = None
     if mode == tf.estimator.ModeKeys.TRAIN:
         optimizer = tf.optimizers.Adam(params.get("learing_rate", 1e-3))
@@ -92,8 +97,7 @@ def model_fn(features, labels, mode, params):
         global_step = tf.compat.v1.train.get_global_step()
         update_global_step = tf.compat.v1.assign(global_step, global_step + 1, name='update_global_step')
         with tf.control_dependencies([update_global_step]):
-          apply_grads = optimizer.apply_gradients(
-              zip(gradient, model.trainable_variables))
+          apply_grads = optimizer.apply_gradients(zip(gradient, model.trainable_variables))
         return apply_grads
 
     if mode == tf.estimator.ModeKeys.TRAIN:
@@ -115,6 +119,7 @@ def main(_):
     config=run_config,
     params={
       "use_tpu": FLAGS.use_tpu,
+      "data_dir": FLAGS.data_dir
     }
   )
   # classifier = tf.estimator.Estimator(model_fn=model_fn, model_dir="mnist/")
@@ -124,6 +129,14 @@ def main(_):
   #    eval_spec=tf.estimator.EvalSpec(input_fn=input_fn),
   #    max_steps=3000
   #)
-  classifier.train(input_fn=lambda params:input_fn(mode=tf.estimator.ModeKeys.TRAIN,**params), max_steps=2000).evaluate(input_fn=lambda params:input_fn(mode=tf.estimator.ModeKeys.EVAL, **params), max_steps=1000)
+  classifier.train(
+          input_fn=lambda params:input_fn(
+              mode=tf.estimator.ModeKeys.TRAIN,
+              **params),
+          max_steps=2000)#.evaluate(
+                  #input_fn=lambda params:input_fn(
+                  #    mode=tf.estimator.ModeKeys.EVAL,
+                  #    **params),
+                  #steps=1000)
 if __name__ == "__main__":
   app.run(main)
